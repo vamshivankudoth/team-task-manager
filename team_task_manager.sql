1d76ae54878fff0a258458149a0f633fe64ccdf2
Vamshi Vankudoth <vamshivankudoth2005@gmail.com>
2026-04-30 16:14:05 +0530

    team_task_manager.sql

--- team_task_manager.sql
+++ team_task_manager.sql
@@ -1,23 +1,26 @@
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100),
-    email VARCHAR(100) UNIQUE,
-    password VARCHAR(255),
-    role ENUM('admin','member')
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role ENUM('admin','member') DEFAULT 'member'
 );
 
 CREATE TABLE projects (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    project_name VARCHAR(100),
-    created_by INT
+    project_name VARCHAR(100) NOT NULL,
+    created_by INT,
+    FOREIGN KEY (created_by) REFERENCES users(id) ON DELETE SET NULL
 );
 
 CREATE TABLE tasks (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(100),
+    title VARCHAR(100) NOT NULL,
     description TEXT,
     assigned_to INT,
     project_id INT,
     status ENUM('Pending','In Progress','Completed') DEFAULT 'Pending',
-    deadline DATE
+    deadline DATE,
+    FOREIGN KEY (assigned_to) REFERENCES users(id) ON DELETE SET NULL,
+    FOREIGN KEY (project_id) REFERENCES projects(id) ON DELETE CASCADE
 );
