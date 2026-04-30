97f11bc4b224723bd872a5671c825f8adba70ded
Vamshi Vankudoth <vamshivankudoth2005@gmail.com>
2026-04-30 16:19:42 +0530

    config.php

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ $pass = getenv("MYSQLPASSWORD");
 $db   = getenv("MYSQLDATABASE");
 
 try {
-    $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    $conn = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (Exception $e) {
     die("Database Connection Failed");
